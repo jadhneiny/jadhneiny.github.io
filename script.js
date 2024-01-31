ae3a60c86da058f9b35defb1c57c003064ea22d1
Jad Hneiny <145118358+jadhneiny@users.noreply.github.com>
2024-01-31 08:55:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Smooth Scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
-
-// Additional JavaScript functionality can be added as needed
