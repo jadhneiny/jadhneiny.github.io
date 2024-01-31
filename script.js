9c565885f4f6b755d4665b90bec29d9b3593300e
Jad Hneiny <145118358+jadhneiny@users.noreply.github.com>
2024-01-31 09:27:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
